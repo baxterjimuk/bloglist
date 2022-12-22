8f43986318383398fa0ef8f15e0177f87058699b
baxterjimuk <81945889+baxterjimuk@users.noreply.github.com>
2022-12-22 21:59:25 +0800

    On branch Tasks-13.19-13.23 Changes to be committed: 	new file:   controllers/reading_lists.js 	modified:   controllers/users.js 	modified:   index.js 	new file:   migrations/20221221_00_reading_list.js 	modified:   models/index.js 	new file:   models/reading_list.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./reading_list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+Blog.belongsToMany(User, { through: ReadingList, as: 'readinglists' })
+User.belongsToMany(Blog, { through: ReadingList, as: 'readings' })
+
 module.exports = {
-  Blog, User
+  Blog, User, ReadingList
 }
